c14e6f497e15baf7d3f1db6bcaaa6edef7a6580c
Ueli Kunz <Ueli.Kunz@finnova.ch>
2015-02-20 13:26:26 +0100

    syntax fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     rules: {
-        "expect-length": require("./lib/rules/expect-length")
+        "expect-length": require("./lib/rules/expect-length"),
         "focused-spec": require("./lib/rules/focused-spec")
     },
     rulesConfig: {
