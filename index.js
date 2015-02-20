328d2223f08263cf825c3da83b31d00e4a89ee2e
Ueli Kunz <Ueli.Kunz@finnova.ch>
2015-02-20 13:25:15 +0100

    add new rule to export

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
     rules: {
         "expect-length": require("./lib/rules/expect-length")
+        "focused-spec": require("./lib/rules/focused-spec")
     },
     rulesConfig: {
     }
