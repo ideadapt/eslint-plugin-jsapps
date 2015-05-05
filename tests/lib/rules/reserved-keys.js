9857bb6ac642b0edba24407ad97ffe027534451e
ideadapt <kunz@ideadapt.net>
2015-05-05 19:10:54 +0200

    only warn if reserved key is directly used on this or self objects

--- tests/lib/rules/reserved-keys.js
+++ tests/lib/rules/reserved-keys.js
@@ -30,12 +30,25 @@ eslintTester.addRuleTest("lib/rules/reserved-keys", {
         {
             args: [1, {keywords: [":)"]}],
             code: "this.id = 1; expect();"
+        },
+        {
+            code: "this.selectedCard.id = 1; expect();"
+        },
+        {
+            code: "other.id = 1; expect();"
         }
     ],
 
     invalid: [
         {
             code: "this.id = 1; expect();",
+            errors: [{
+                message: "Property name 'id' is a reserved identifier name. It might be used by another framework in this context.",
+                type: "MemberExpression"
+            }]
+        },
+        {
+            code: "self.id = 1; expect();",
             errors: [{
                 message: "Property name 'id' is a reserved identifier name. It might be used by another framework in this context.",
                 type: "MemberExpression"
